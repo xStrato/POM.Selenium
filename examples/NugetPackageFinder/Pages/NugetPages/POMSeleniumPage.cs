cf3193a0576495395f5dead9eeeae2598a435156
xStrato <metalofgear@hotmail.com>
2021-01-25 00:20:28 -0300

    added examples folder and PackageFinder example

---
+++ examples/NugetPackageFinder/Pages/NugetPages/POMSeleniumPage.cs
@@ -0,0 +1,12 @@
+﻿
+using OpenQA.Selenium;
+using POM.Selenium.Entities;
+using POM.Selenium.Entities.Pages;
+
+namespace NugetPackageFinder.Pages.NugetPages
+{
+    public class POMSeleniumPage : Page<POMSeleniumPage>
+    {
+        public POMSeleniumPage(IWebDriver driver, Selector selector) : base(driver, selector){}
+    }
+}
